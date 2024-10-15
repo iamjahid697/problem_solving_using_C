3d73e4f402d4c609d33fa66ce73784e4d0a73a21
iamjahid697 <iamjahid697@gmail.com>
2024-10-16 00:42:45 +0600

    codeforces problem link

--- codeforces/nChar.c
+++ codeforces/nChar.c
@@ -1,12 +1,14 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/N
+
 #include<stdio.h>
 int main(){
     char ch;
     scanf("%c",&ch);
     if(ch >= 'a' && ch <= 'z'){
-        ch -= 32;
+        ch -= 32; //lowercase
     }
     else{
-        ch +=32;
+        ch +=32; //uppercase
     }
     printf("%c\n", ch);
     return 0;
